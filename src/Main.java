bed2d56dc11792e1542567badb5cd8252d948faa
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2022-01-09 13:41:48 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 5000;
+        int percent = 20;
+        int bonus = amount / percent;
+        System.out.println(bonus);
+
+
+    }
+}
