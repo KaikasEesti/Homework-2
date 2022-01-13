5f2d647cc1d222f7c3e461e2fa9efd8bc437a19e
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2022-01-13 16:47:03 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 5000;
-        int percent = 20;
-        int bonus = amount / percent;
-        System.out.println(bonus);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate (price);
+        System.out.println(miles);
+    }
 
     }
-}
+
+
